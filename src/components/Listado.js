cbbc3e62066a958ae9d6bce65f133f6a4ba6fee3
marcemrze24 <mercadomarcelo2@hotmail.com>
2023-02-17 20:09:03 -0300

    some tweaks

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -3,9 +3,22 @@ import { Link, Navigate } from "react-router-dom";
 
 // React-Bootstrap
 import { Card, Col, Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Listado = () => {
     const token = localStorage.getItem("token");
+    const endPoint =
+        "https://api.themoviedb.org/3/discover/movie?api_key=78e3f972ec6c669fc1ac630ff32f3a2d&language=es-ES";
+    const [moviesList, setMoviesList] = useState([]);
+
+    // Integracion de la API en montaje
+    useEffect(() => {
+        axios.get(endPoint).then((resp) => {
+            const apiData = resp.data;
+            setMoviesList(apiData.results);
+        });
+    }, [moviesList]);
 
     // Renderizado condicional, dependiente del "token"
     if (!token) {
@@ -13,63 +26,29 @@ const Listado = () => {
     }
     return (
         <Row xs={1} className="w-75 mx-auto gap-4">
-            <Col>
-                <Card>
-                    <Card.Img
-                        src="https://picsum.photos/250/200"
-                        variant="top"
-                    />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            {" "}
-                            Some quick example text to build on the card title
-                            and make up the{" "}
-                        </Card.Text>
-                        <Link as="button" className="btn btn-primary" to={"/"}>
-                            Click me!
-                        </Link>
-                    </Card.Body>
-                </Card>
-            </Col>
-            <Col>
-                <Card>
-                    <Card.Img
-                        src="https://picsum.photos/250/200"
-                        variant="top"
-                    />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            {" "}
-                            Some quick example text to build on the card title
-                            and make up the{" "}
-                        </Card.Text>
-                        <Link as="button" className="btn btn-primary" to={"/"}>
-                            Click me!
-                        </Link>
-                    </Card.Body>
-                </Card>
-            </Col>
-            <Col>
-                <Card>
-                    <Card.Img
-                        src="https://picsum.photos/250/200"
-                        variant="top"
-                    />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            {" "}
-                            Some quick example text to build on the card title
-                            and make up the{" "}
-                        </Card.Text>
-                        <Link as="button" className="btn btn-primary" to={"/"}>
-                            Click me!
-                        </Link>
-                    </Card.Body>
-                </Card>
-            </Col>
+            {moviesList.map((movie, idx) => {
+                return (
+                    <Col key={idx}>
+                        <Card>
+                            <Card.Img
+                                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                                variant="top"
+                            />
+                            <Card.Body>
+                                <Card.Title>{movie.title}</Card.Title>
+                                <Card.Text>{movie.overview}</Card.Text>
+                                <Link
+                                    as="button"
+                                    className="btn btn-primary"
+                                    to={"/"}
+                                >
+                                    Click me!
+                                </Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                );
+            })}
         </Row>
     );
 };
